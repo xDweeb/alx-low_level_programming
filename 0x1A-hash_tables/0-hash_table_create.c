7feee0d8c5e57f72544b34ec8b5c556d1279a244
Taibi El Yakouti <elyakouti999@hotmail.com>
2023-12-24 13:45:58 -0800

    xdweb

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,30 +2,25 @@
 
 /**
  * hash_table_create - Creates a hash table.
- * @size: the size, in number of nodes, to make the new hash table.
+ * @size: The size of the array.
  *
- * Return: a pointer to the newly created hash table.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned int i = 0;
-	hash_table_t *ht = malloc(sizeof(hash_table_t));
+	hash_table_t *ht;
+	unsigned long int i;
 
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	}
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
 		return (NULL);
-	}
-
-	for (; i < size; i++)
+	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
 	return (ht);
