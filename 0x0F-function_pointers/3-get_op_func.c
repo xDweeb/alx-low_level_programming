d8e361bb1fc527ee3369262f59bda4e7c6979c48
Taibi El Yakouti <elyakouti999@hotmail.com>
2024-04-24 06:47:21 -0700

    xd

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,36 +1,28 @@
 #include "3-calc.h"
 #include <stdlib.h>
-#include <string.h>
-
-
 /**
-* get_op_func - ...
-* @s: ...
-*
-* Return: ...
-*/
+ * get_op_func - Selects the correct function to perform
+ *               the operation asked by the user.
+ * @s: The operator passed as argument.
+ *
+ * Return: A pointer to the function corresponding
+ *         to the operator given as a parameter.
+ */
 int (*get_op_func(char *s))(int, int)
 {
-   op_t ops[] = {
-       {"+", op_add},
-       {"-", op_sub},
-       {"*", op_mul},
-       {"/", op_div},
-       {"%", op_mod},
-       {NULL, NULL}};
-   int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
 
+	int i = 0;
 
-   while (i < 5)
-   {
-       if (strcmp(s, ops[i].op) == 0)
-           return (ops[i].f);
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
+		i++;
 
-
-       i++;
-   }
-
-
-   return (0);
+	return (ops[i].f);
 }
-
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,83 +1,63 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 
 /**
-* op_add - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
+ * op_add - Returns the sum of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The sum of a and b.
+ */
 int op_add(int a, int b)
 {
-   return (a + b);
+	return (a + b);
 }
-
-
 /**
-* op_sub - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
+ * op_sub - Returns the difference of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The difference of a and b.
+ */
 int op_sub(int a, int b)
 {
-   return (a - b);
+	return (a - b);
 }
-
-
 /**
-* op_mul - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
+ * op_mul - Returns the product of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The product of a and b.
+ */
 int op_mul(int a, int b)
 {
-   return (a * b);
+	return (a * b);
 }
-
-
 /**
-* op_div - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
+ * op_div - Returns the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The quotient of a and b.
+ */
 int op_div(int a, int b)
 {
-   if (b == 0)
-   {
-       printf("Error\n");
-       exit(100);
-   }
-
-
-   return (a / b);
+	return (a / b);
 }
-
-
 /**
-* op_mod - ...
-* @a: ...
-* @b: ...
-*
-* Return: ...
-*/
+ * op_mod - Returns the remainder of the division of two numbers.
+ * @a: The first number.
+ * @b: The second number.
+ *
+ * Return: The remainder of the division of a by b.
+ */
 int op_mod(int a, int b)
 {
-   if (b == 0)
-   {
-       printf("Error\n");
-       exit(100);
-   }
-
-
-   return (a % b);
+	return (a % b);
 }
-
